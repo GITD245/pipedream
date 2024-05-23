9b6fc1ff097976d1a00f00846fea4a889d3db359
GITD245 <2456388197@qq.com>
2024-05-23 15:25:48 +0800

    add pipedream segmentation

---
+++ runtime/image_classification/models/vgg16/gpus=8_pipedream/stage1.py
@@ -0,0 +1,37 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import torch
+
+
+class Stage1(torch.nn.Module):
+    def __init__(self):
+        super(Stage1, self).__init__()
+        self.layer17 = torch.nn.ReLU()
+        self.layer18 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.layer19 = torch.nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.layer20 = torch.nn.ReLU()
+        self.layer21 = torch.nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    torch.nn.init.constant_(m.bias, 0)
+            elif isinstance(m, torch.nn.BatchNorm2d):
+                torch.nn.init.constant_(m.weight, 1)
+                torch.nn.init.constant_(m.bias, 0)
+            elif isinstance(m, torch.nn.Linear):
+                torch.nn.init.normal_(m.weight, 0, 0.01)
+                torch.nn.init.constant_(m.bias, 0)
+
+    def forward(self, input0):
+        out16 = input0.clone()
+        out17 = self.layer17(out16)
+        out18 = self.layer18(out17)
+        out19 = self.layer19(out18)
+        out20 = self.layer20(out19)
+        out21 = self.layer21(out20)
+        return out21
