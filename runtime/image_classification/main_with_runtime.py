f90374f22718772bd596f2ad8b8ece24689fe132
GITD245 <2456388197@qq.com>
2024-05-23 21:59:06 +0800

    test

--- runtime/image_classification/main_with_runtime.py
+++ runtime/image_classification/main_with_runtime.py
@@ -456,7 +456,8 @@ def validate(val_loader, r, epoch):
     n = r.num_iterations(loader_size=len(val_loader))
     if args.num_minibatches is not None:
         n = min(n, args.num_minibatches)
-    if not is_first_stage(): n=args.laststage_n
+    print(f'validate_n:{n}')
+    # if not is_first_stage(): n=args.laststage_n
     r.eval(n)
     if not is_first_stage(): val_loader = None
     r.set_loader(val_loader)
