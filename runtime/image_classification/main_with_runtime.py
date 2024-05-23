3fce334a9a54f6381b4b125d95504bbb8f0b153e
GITD245 <2456388197@qq.com>
2024-05-23 22:30:16 +0800

    fix n bug

--- runtime/image_classification/main_with_runtime.py
+++ runtime/image_classification/main_with_runtime.py
@@ -324,7 +324,7 @@ def main():
         if args.forward_only:
             validate(val_loader, r, epoch)
         else:
-            # train(train_loader, r, optimizer, epoch)
+            train(train_loader, r, optimizer, epoch)
 
             # evaluate on validation set
             print(len(val_dataset))
